5baf1005b4297e8b3279e58d3aae3817be3c2542
Justin Wilson <justinwilson1@gmail.com>
2014-07-16 13:06:51 -0600

    added ThenFunc

--- chain.go
+++ chain.go
@@ -60,6 +60,28 @@ func (c Chain) Then(h http.Handler) http.Handler {
 	return final
 }
 
+// Then chains the middleware and returns the final http.Handler.
+//     New(m1, m2, m3).ThenFunc(h)
+// is equivalent to:
+//     m1(m2(m3(http.HandlerFunc(h))))
+// When the request comes in, it will be passed to m1, then m2, then m3
+// and finally, the given handler
+// (assuming every middleware calls the following one).
+//
+// A chain can be safely reused by calling ThenFunc() several times.
+//     stdStack := alice.New(ratelimitHandler, csrfHandler)
+//     indexPipe = stdStack.ThenFunc(indexHandlerFunc)
+//     authPipe = stdStack.ThenFunc(authHandlerFunc)
+// Note that constructors are called on every call to Then()
+// and thus several instances of the same middleware will be created
+// when a chain is reused in this way.
+// For proper middleware, this should cause no problems.
+//
+// ThenFunc() treats nil as http.DefaultServeMux.
+func (c Chain) ThenFunc(fn http.HandlerFunc) http.Handler {
+	return c.Then(http.HandlerFunc(fn))
+}
+
 // Append extends a chain, adding the specified constructors
 // as the last ones in the request flow.
 //
